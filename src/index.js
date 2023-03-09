df81b4181a575046f92a8acfd4c2df014d8c4ff0
asuo1986 <asuo1986@gmail.com>
2023-03-09 09:47:17 -0500

    first commit, check in all node_modules packages

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const element = document.getElementById('root');
+
+const root = ReactDOM.createRoot(element);
+
+const App = () => {
+    return <div>Hello BEAR Lettering Module！</div>;
+};
+
+root.render(<App />);
